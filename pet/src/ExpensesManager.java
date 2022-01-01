e0b32f687a38b091945ddb9f22d39e5078db1039
ashenone23 <91793116+ashenone23@users.noreply.github.com>
2022-01-01 03:01:21 +0300

    Add files via upload

---
+++ pet/src/ExpensesManager.java
@@ -0,0 +1,31 @@
+class ExpensesManager{
+    double[] expenses;
+    double saveExpense(double moneyBeforeSalary, double expense, int day) {
+
+        // Печать вопросов и считывание ответов оставьте в классе Praktikum
+
+
+        moneyBeforeSalary = moneyBeforeSalary - expense;
+        expenses[day - 1] = expenses[day - 1] + expense;
+        System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
+        return moneyBeforeSalary;
+    }
+    double findMaxExpense() {
+        double maxExpense = 0;
+        for (int i = 0; i < expenses.length; i++) {
+            if (expenses[i] > maxExpense) {
+                maxExpense = expenses[i];
+            }
+        }
+        return maxExpense;
+    }
+    void printAllExpenses() {
+        for (int i = 0; i < expenses.length; i++) {
+            System.out.println("День " + (i + 1) + ". Потрачено " + expenses[i] + " рублей");
+        }
+    }
+
+    ExpensesManager(){
+        expenses = new double[7];
+    }
+}
